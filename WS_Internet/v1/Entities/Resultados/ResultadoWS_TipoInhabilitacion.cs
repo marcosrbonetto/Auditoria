8becf42a993d3a9b34a7fbc17e4a2d41886183d9
Marcos Bonetto <sasia_r@cordoba.gov.ar>
2019-04-16 11:51:27 -0300

    controller tipo inhab

---
+++ WS_Internet/v1/Entities/Resultados/ResultadoWS_TipoInhabilitacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WS_Internet.v1.Entities.Resultados
+{
+    [Serializable]
+    public class ResultadoWS_TipoInhabilitacion
+    {
+        public int KeyValue { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ WS_Intranet/v1/Entities/Resultados/ResultadoWS_TipoInhabilitacion.cs
@@ -0,0 +1,28 @@
+﻿using _Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WS_Intranet.v1.Entities.Resultados
+{
+    [Serializable]
+    public class ResultadoWS_TipoInhabilitacion
+    {
+        public int KeyValue { get; set; }
+        public string Nombre { get; set; }
+
+        public ResultadoWS_TipoInhabilitacion(TipoInhabilitacion entity)
+        {
+            if (entity == null) return;
+
+            Nombre = entity.Nombre;
+            KeyValue = (int)entity.KeyValue;
+        }
+
+        public static List<ResultadoWS_TipoInhabilitacion> ToList(IList<TipoInhabilitacion> list)
+        {
+            return list.Select(x => new ResultadoWS_TipoInhabilitacion(x)).ToList();
+
+        }
+    }
+}
