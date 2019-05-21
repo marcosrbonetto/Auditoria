a0b38b29d57742694a9bf69312ae1a348d203d4f
Bagnus Marcio <mbagnus@cordoba.gov.ar>
2019-05-21 17:03:42 -0300

    reportes

--- _Rules/Rules/Reportes/Rules_Reportes.cs
+++ _Rules/Rules/Reportes/Rules_Reportes.cs
@@ -29,6 +29,172 @@ namespace _Rules.Rules
             return dao.GetByKeyValue(keyValue);
         }
 
+        //public Resultado<string> GetInscripcionesPorDni(_Model.Consultas.Consulta_Inscripcion consulta)
+        //{
+        //    var resultado = new Resultado<string>();
+        //    var resultadoInscripcion = _InscripcionRules.Get(consulta);
+        //    if (!resultadoInscripcion.Ok)
+        //    {
+        //        resultado.Error = resultadoInscripcion.Error;
+        //        return resultado;
+        //    }
+        //    var inscripcion = resultadoInscripcion.Return;
+
+        //    try
+        //    {
+        //        //var rutaReporte = HttpContext.Current.Server.MapPath("~/");
+        //        var rutaReportePadre = new DirectoryInfo(HttpContext.Current.Server.MapPath("~/")).Parent.FullName;
+
+        //        var rutaReporte = rutaReportePadre + "/_Rules/Resources/Reportes/Reporte_Inscripcion_Dni.trdx";
+
+        //        Telerik.Reporting.Report reporte = new Telerik.Reporting.Report();
+        //        System.Xml.XmlReaderSettings settings = new System.Xml.XmlReaderSettings();
+        //        settings.IgnoreWhitespace = true;
+
+        //        System.Xml.XmlReader xmlReaderRequerimiento = System.Xml.XmlReader.Create(rutaReporte, settings);
+        //        Telerik.Reporting.XmlSerialization.ReportXmlSerializer xmlSerializerRequerimiento = new Telerik.Reporting.XmlSerialization.ReportXmlSerializer();
+        //        reporte = (Telerik.Reporting.Report)xmlSerializerRequerimiento.Deserialize(xmlReaderRequerimiento);
+
+        //        //---------------------------------
+        //        // Defino el DT
+        //        //---------------------------------
+
+        //        Telerik.Reporting.ObjectDataSource objectDataSourceInscripcion = new Telerik.Reporting.ObjectDataSource();
+        //        DataTable dtInscripcion = new DataTable();
+
+        //        //Campos
+        //        dtInscripcion.Columns.Add("Nombre");
+        //        dtInscripcion.Columns.Add("Apellido");
+        //        dtInscripcion.Columns.Add("Documento");
+        //        dtInscripcion.Columns.Add("DomicilioCalle");
+        //        dtInscripcion.Columns.Add("DomicilioAltura");
+        //        dtInscripcion.Columns.Add("DomicilioBarrio");
+
+        //        dtInscripcion.Columns.Add("TipoAuto");
+        //        dtInscripcion.Columns.Add("idTipoAuto");
+        //        dtInscripcion.Columns.Add("identificador");
+
+
+        //        //-------------------------------------
+        //        // Creo una fila y le cargo los datos
+        //        //-------------------------------------
+
+        //        DataRow filaInscripcion = dtInscripcion.NewRow();
+
+        //        //****************Encabezado*********************************
+        //        //****************USUARIO********************************
+        //        Usuario usuario = inscripcion[0].Usuario;
+        //        //Nombre
+        //        string nombre = usuario.Nombre;
+        //        if (string.IsNullOrEmpty(nombre))
+        //        {
+        //            nombre = "Sin Datos";
+        //        }
+        //        filaInscripcion["Nombre"] = nombre;
+
+        //        //Apellido
+        //        string apellido = usuario.Apellido;
+        //        if (string.IsNullOrEmpty(nombre))
+        //        {
+        //            apellido = "Sin Datos";
+        //        }
+        //        filaInscripcion["Apellido"] = apellido;
+
+        //        //Dni
+        //        string dni = usuario.Dni.ToString();
+        //        if (string.IsNullOrEmpty(dni))
+        //        {
+        //            dni = "Sin Datos";
+        //        }
+        //        filaInscripcion["Documento"] = dni;
+
+        //        //Calle
+        //        string calle = usuario.DomicilioCalle;
+        //        if (string.IsNullOrEmpty(calle))
+        //        {
+        //            calle = "Sin Datos";
+        //        }
+        //        filaInscripcion["DomicilioCalle"] = calle;
+        //        //Altura
+        //        string altura = usuario.DomicilioAltura.ToString();
+        //        if (string.IsNullOrEmpty(altura))
+        //        {
+        //            altura = "Sin Datos";
+        //        }
+        //        filaInscripcion["DomicilioAltura"] = altura;
+        //        //Barrio
+        //        string barrio = usuario.DomicilioBarrio;
+        //        if (string.IsNullOrEmpty(barrio))
+        //        {
+        //            barrio = "Sin Datos";
+        //        }
+        //        filaInscripcion["DomicilioBarrio"] = barrio;
+        //        //***************TABLA FECHAS********************************
+
+        //        DataTable dtFechas = new DataTable();
+        //        dtFechas.Columns.Add("FechaInicio");
+        //        dtFechas.Columns.Add("FechaFin");
+        //        dtFechas.Columns.Add("FechaTelegrama");
+
+
+        //        foreach (Inscripcion i in inscripcion)
+        //        {
+        //            DataRow filaFechas = dtFechas.NewRow();
+
+        //            filaInscripcion["TipoAuto"] = i.TipoAuto.Nombre;
+        //            filaInscripcion["idTipoAuto"] = i.TipoAuto.Id;
+        //            filaInscripcion["identificador"] = i.Identificador;
+
+
+
+        //            var fechaInicio = i.FechaInicio;
+        //            if (!string.IsNullOrEmpty(fechaInicio.ToString()))
+        //            {
+        //                filaFechas["FechaInicio"] = i.FechaInicio.Value.ToString("dd/MM/yyyy");
+        //            }
+
+        //            var fechaFin = i.FechaFin;
+        //            if (!string.IsNullOrEmpty(fechaFin.ToString()))
+        //            {
+        //                filaFechas["FechaFin"] = i.FechaFin.Value.ToString("dd/MM/yyyy");
+        //            }
+
+        //            var fechaTelegrama = i.FechaTelegrama;
+        //            if (!string.IsNullOrEmpty(fechaTelegrama.ToString()))
+        //            {
+        //                filaFechas["FechaTelegrama"] = fechaTelegrama.Value.ToString("dd/MM/yyyy");
+        //            }
+
+
+        //            dtFechas.Rows.Add(filaFechas);
+        //            //dtDescripcion.Rows.Add(filaDescripcion);
+        //        }
+        //        Telerik.Reporting.Table tFechas = (Telerik.Reporting.Table)(reporte.Items.Find("tablaFechas", true)[0]);
+        //        tFechas.DataSource = dtFechas;
+
+        //        //Agrego la fila del requerimiento
+        //        dtInscripcion.Rows.Add(filaInscripcion);
+
+        //        //Seteo el datasource al reclamo
+        //        objectDataSourceInscripcion.DataSource = dtInscripcion;
+        //        reporte.DataSource = objectDataSourceInscripcion;
+
+        //        Telerik.Reporting.Processing.ReportProcessor reportProcessor = new ReportProcessor();
+        //        RenderingResult result = reportProcessor.RenderReport("PDF", reporte, null);
+        //        var bytes = result.DocumentBytes;
+        //        var base64 = "data:application/pdf;base64," + Convert.ToBase64String(bytes);
+        //        resultado.Return = base64;
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        resultado.Error = "Error procesando la solicitud";
+        //        return resultado;
+        //    }
+
+        //    return resultado;
+        //}
+
         public Resultado<string> GetInscripcionesPorDni(_Model.Consultas.Consulta_Inscripcion consulta)
         {
             var resultado = new Resultado<string>();
@@ -38,14 +204,14 @@ namespace _Rules.Rules
                 resultado.Error = resultadoInscripcion.Error;
                 return resultado;
             }
-            var inscripcion = resultadoInscripcion.Return;
+            var inscripciones = resultadoInscripcion.Return;
 
             try
             {
                 //var rutaReporte = HttpContext.Current.Server.MapPath("~/");
                 var rutaReportePadre = new DirectoryInfo(HttpContext.Current.Server.MapPath("~/")).Parent.FullName;
-                
-                var rutaReporte = rutaReportePadre+"/_Rules/Resources/Reportes/Reporte_Inscripcion_Dni.trdx";
+
+                var rutaReporte = rutaReportePadre + "/_Rules/Resources/Reportes/Reporte_Inscripcion_Dni_encabezado.trdx";
 
                 Telerik.Reporting.Report reporte = new Telerik.Reporting.Report();
                 System.Xml.XmlReaderSettings settings = new System.Xml.XmlReaderSettings();
@@ -60,17 +226,16 @@ namespace _Rules.Rules
                 //---------------------------------
 
                 Telerik.Reporting.ObjectDataSource objectDataSourceInscripcion = new Telerik.Reporting.ObjectDataSource();
-                DataTable dtInscripcion = new DataTable();
+                DataTable dtInscripcionUsuario = new DataTable();
 
                 //Campos
-                dtInscripcion.Columns.Add("Nombre");
-                dtInscripcion.Columns.Add("Apellido");
-                dtInscripcion.Columns.Add("Documento");
-                dtInscripcion.Columns.Add("DomicilioCalle");
-                dtInscripcion.Columns.Add("DomicilioAltura");
-                dtInscripcion.Columns.Add("DomicilioBarrio");
-                dtInscripcion.Columns.Add("TipoAuto");
-                dtInscripcion.Columns.Add("Identificador");
+                dtInscripcionUsuario.Columns.Add("Nombre");
+                dtInscripcionUsuario.Columns.Add("Apellido");
+                dtInscripcionUsuario.Columns.Add("Documento");
+                dtInscripcionUsuario.Columns.Add("DomicilioCalle");
+                dtInscripcionUsuario.Columns.Add("DomicilioAltura");
+                dtInscripcionUsuario.Columns.Add("DomicilioBarrio");
+
 
 
 
@@ -78,11 +243,11 @@ namespace _Rules.Rules
                 // Creo una fila y le cargo los datos
                 //-------------------------------------
 
-                DataRow filaInscripcion = dtInscripcion.NewRow();
+                DataRow filaInscripcion = dtInscripcionUsuario.NewRow();
 
                 //****************Encabezado*********************************
                 //****************USUARIO********************************
-                Usuario usuario = inscripcion[0].Usuario;
+                Usuario usuario = inscripciones[0].Usuario;
                 //Nombre
                 string nombre = usuario.Nombre;
                 if (string.IsNullOrEmpty(nombre))
@@ -106,7 +271,7 @@ namespace _Rules.Rules
                     dni = "Sin Datos";
                 }
                 filaInscripcion["Documento"] = dni;
-                
+
                 //Calle
                 string calle = usuario.DomicilioCalle;
                 if (string.IsNullOrEmpty(calle))
@@ -128,50 +293,17 @@ namespace _Rules.Rules
                     barrio = "Sin Datos";
                 }
                 filaInscripcion["DomicilioBarrio"] = barrio;
-                //***************DESCRIPCION********************************
-                //Tipo Auto
-                string tipoAuto = inscripcion[0].TipoAuto.Nombre;
-                if (string.IsNullOrEmpty(tipoAuto))
-                {
-                    tipoAuto = "Sin Datos";
-                }
-                filaInscripcion["TipoAuto"] = tipoAuto;
 
-                //Chapa
-                string chapa = inscripcion[0].Identificador;
-                if (string.IsNullOrEmpty(chapa))
-                {
-                    chapa = "Sin Datos";
-                }
-                filaInscripcion["Identificador"] = chapa;
-                //***************TABLA FECHAS********************************
 
-                DataTable dtFechas = new DataTable();
-                dtFechas.Columns.Add("FechaInicio");
-                dtFechas.Columns.Add("FechaFin");
-                dtFechas.Columns.Add("FechaTelegrama");
-                
-                foreach (Inscripcion i in inscripcion)
-                {
-                    DataRow filaFechas = dtFechas.NewRow();
-                    filaFechas["FechaInicio"] = i.FechaInicio.Value.ToString("dd/MM/yyyy");
-                    filaFechas["FechaFin"] = i.FechaFin.Value.ToString("dd/MM/yyyy");
-                    filaFechas["FechaTelegrama"] = i.FechaTelegrama.Value.ToString("dd/MM/yyyy");                    
-                    dtFechas.Rows.Add(filaFechas);
-                }
-                Telerik.Reporting.Table tFechas = (Telerik.Reporting.Table)(reporte.Items.Find("tablaFechas", true)[0]);
-                tFechas.DataSource = dtFechas;
+                Telerik.Reporting.SubReport subReportItem = (Telerik.Reporting.SubReport)(reporte.Items.Find("subreporteInsripciones", true)[0]);
+                subReportItem.ReportSource = crearSubreporteInscripciones(inscripciones).Return;
 
                 //Agrego la fila del requerimiento
-                dtInscripcion.Rows.Add(filaInscripcion);
+                dtInscripcionUsuario.Rows.Add(filaInscripcion);
 
                 //Seteo el datasource al reclamo
-                objectDataSourceInscripcion.DataSource = dtInscripcion;
+                objectDataSourceInscripcion.DataSource = dtInscripcionUsuario;
                 reporte.DataSource = objectDataSourceInscripcion;
-
-                //resultado.Return = reporte;         
-
-
 
                 Telerik.Reporting.Processing.ReportProcessor reportProcessor = new ReportProcessor();
                 RenderingResult result = reportProcessor.RenderReport("PDF", reporte, null);
@@ -187,6 +319,90 @@ namespace _Rules.Rules
             }
 
             return resultado;
+        }
+
+        public Resultado<Telerik.Reporting.Report> crearSubreporteInscripciones(List<Inscripcion> inscripciones)
+        {
+            var result = new Resultado<Telerik.Reporting.Report>();
+
+            try
+            {
+                //var rutaReporte = HttpContext.Current.Server.MapPath("~/");
+                var rutaReportePadre = new DirectoryInfo(HttpContext.Current.Server.MapPath("~/")).Parent.FullName;
+
+                var rutaReporte = rutaReportePadre + "/_Rules/Resources/Reportes/Reporte_Inscripcion_Dni_detalle.trdx";
+
+                Telerik.Reporting.Report reporte = new Telerik.Reporting.Report();
+                System.Xml.XmlReaderSettings settings = new System.Xml.XmlReaderSettings();
+                settings.IgnoreWhitespace = true;
+
+                System.Xml.XmlReader xmlReaderRequerimiento = System.Xml.XmlReader.Create(rutaReporte, settings);
+                Telerik.Reporting.XmlSerialization.ReportXmlSerializer xmlSerializerRequerimiento = new Telerik.Reporting.XmlSerialization.ReportXmlSerializer();
+                reporte = (Telerik.Reporting.Report)xmlSerializerRequerimiento.Deserialize(xmlReaderRequerimiento);
+
+                //---------------------------------
+                // Defino el DT
+                //---------------------------------
+
+                Telerik.Reporting.ObjectDataSource objectDataSourceInscripcion = new Telerik.Reporting.ObjectDataSource();
+                DataTable dtInscripcion = new DataTable();
+
+                //Campos
+                dtInscripcion.Columns.Add("TipoAuto");
+                dtInscripcion.Columns.Add("idTipoAuto");
+                dtInscripcion.Columns.Add("identificador");
+                dtInscripcion.Columns.Add("FechaInicio");
+                dtInscripcion.Columns.Add("FechaFin");
+                dtInscripcion.Columns.Add("FechaTelegrama");
+                
+                foreach (var i in inscripciones)
+                {
+                    //-------------------------------------
+                    // Creo una fila y le cargo los datos
+                    //-------------------------------------
+
+                    DataRow filaInscripcion = dtInscripcion.NewRow();
+
+                    filaInscripcion["TipoAuto"] = i.TipoAuto.Nombre;
+                    filaInscripcion["idTipoAuto"] = i.TipoAuto.Id;
+                    filaInscripcion["identificador"] = i.Identificador;
+
+
+                                  
+                    var fechaInicio = i.FechaInicio;
+                    if (!string.IsNullOrEmpty(fechaInicio.ToString()))
+                    {
+                        filaInscripcion["FechaInicio"] = i.FechaInicio.Value.ToString("dd/MM/yyyy");
+                    }
+
+                    var fechaFin = i.FechaFin;
+                    if (!string.IsNullOrEmpty(fechaFin.ToString()))
+                    {
+                        filaInscripcion["FechaFin"] = i.FechaFin.Value.ToString("dd/MM/yyyy");
+                    }
+
+                    var fechaTelegrama = i.FechaTelegrama;
+                    if (!string.IsNullOrEmpty(fechaTelegrama.ToString()))
+                    {
+                        filaInscripcion["FechaTelegrama"] = fechaTelegrama.Value.ToString("dd/MM/yyyy");
+                    }
+
+                    //Agrego la fila del requerimiento
+                    dtInscripcion.Rows.Add(filaInscripcion);                   
+                }            
+
+                //Seteo el datasource al reclamo
+                objectDataSourceInscripcion.DataSource = dtInscripcion;
+                reporte.DataSource = objectDataSourceInscripcion;
+
+                result.Return = reporte;
+            }
+            catch (Exception e)
+            {
+                result.Error = "Error procesando la solicitud";
+                return result;
+            }
+            return result;
         }
 
 
